f1207d568153c25ccc1b1407bec272302d0f7184
chinmayto <chinmay13@gmail.com>
2026-04-02 23:08:59 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -8,7 +8,14 @@ terraform {
     }
   }
 
-  backend "s3" {}
+  backend "s3" {
+    bucket                     = "chinmayto-terraform-state-bucket-1755526674"
+    key                        = "terraform-ci-cd/terraform.tfstate"
+    region                     = "us-east-1"
+    encrypt                    = true
+    use_lockfile               = true
+    skip_requesting_account_id = false
+  }
 }
 
 provider "aws" {
